e5a465a1fcd771653f842c1cb7d25c6347856786
LuizAndrade <lcfsandrade@gmail.com>
2024-04-11 19:21:26 -0300

    Updated weddings controller

--- app/controllers/weddings_controller.rb
+++ app/controllers/weddings_controller.rb
@@ -1,52 +1,7 @@
 class WeddingsController < ApplicationController
   before_action :set_wedding, only: %i[edit update destroy]
   skip_before_action :authenticate_user!, only: [:show]
-
-  def show
-    @my = Wedding.last
-    @wedding = Wedding.find(@my.id)
-    @gifts = Gift.where(wedding: @wedding)
-    @guests = Guest.where(wedding: @wedding)
-    @user_wedding = Wedding.where(user: @wedding.user_id).to_a
-    @user_wedding_tips = Tip.where(wedding: @user_wedding)
-    @wedding_guests_messages = Guest.where(wedding: @wedding).select { |guest| guest.confirmation_message.present? }
-    @partner_name = @user_wedding.map { |partner| "#{ partner.partner_first_name }" }.first
-    @days_left = (@wedding.date - Date.today).to_i
-    @month = @wedding.date.strftime("%B")
-    @month =
-    case @month
-      when "January" then "Janeiro"
-      when "February" then "Fevereiro"
-      when "March" then "Março"
-      when "April" then "Abril"
-      when "May" then "Maio"
-      when "June" then "Junho"
-      when "July" then "Julho"
-      when "August" then "Agosto"
-      when "September" then "Setembro"
-      when "October" then "Outubro"
-      when "November" then "Novembro"
-      when "December" then "Dezembro"
-    end
-    @day = @wedding.date.strftime("%d")
-    @year = @wedding.date.strftime("%Y")
-    @date = "#{@day} de #{@month}, #{@year} | #{@wedding.time.strftime("%H:%M")}h"
-    @markers = [{
-                lat: @wedding.latitude,
-                lng: @wedding.longitude,
-                info_window_html: render_to_string(
-                                                  partial: "info_window",
-                                                  locals: { wedding: @wedding }),
-                marker_html: render_to_string(partial: "marker")
-                                                  }] if @wedding.latitude && @wedding.longitude
-
-    if @wedding
-      render :show
-    else
-      redirect_to root_path
-    end
-  end
-
+  
   def new
     @wedding = Wedding.new
   end
