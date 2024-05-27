be0b2c902a947d65f51bfa67f2881cd70d8833c6
LuizAndrade <lcfsandrade@gmail.com>
2024-05-27 14:51:23 -0300

    Class guest fix

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,7 +1,7 @@
 class Guest < ApplicationRecord
   belongs_to :wedding
 
-  has_many :companions, class_name: "Guest", foreign_key: "primary_guest_id", inverse_of: :primary_guest
+  has_many :companions, class_name: "Guest", foreign_key: "primary_guest_id", inverse_of: :primary_guest, dependent: :destroy
   belongs_to :primary_guest, class_name: "Guest", inverse_of: :companions, optional: true
 
   validates :full_name, presence: true, length: { minimum: 3, maximum: 50 }
